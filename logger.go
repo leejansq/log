49063c8acf8676ba94f8bec43a22381ae9def263
leejan <lj176172@alibaba-inc.com>
2020-07-04 14:04:14 +0800

    first commit

---
+++ logger.go
@@ -0,0 +1,16 @@
+package log
+
+import "github.com/Sirupsen/logrus"
+
+type Entry struct {
+	*logrus.Entry
+}
+
+func (l *Entry) Child() *Entry {
+	entry := *l.Entry
+	return &Entry{&entry}
+}
+
+func New() *Entry {
+	return &Entry{logrus.NewEntry(logrus.New())}
+}
